73b308952cf90ca97abd2029f639e0d4e8d28f79
MiraVoronina <voroninaslava23@gmail.com>
2025-02-20 15:51:57 +0700

    3

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        columns: [[], [], []]
+        columns: [[], [], []],
+        nextId: 1
+    },
+    methods: {
+        addCard() {
+            if (this.columns[0].length >= 3) return;
+
+            this.columns[0].push({
+                id: this.nextId++,
+                title: 'Новая карточка'
+            });
+        }
     }
 });
